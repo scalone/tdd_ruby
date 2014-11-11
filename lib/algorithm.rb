b73f898deb866373144fd8abbffc64f01eee1eae
Thiago Scalone <thiago@scalone.com.br>
2014-11-10 23:17:28 -0200

    Implement square and xpto.

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,4 +1,9 @@
 class Algorithm
-  def self.square
+  def self.square(number)
+    number * number
+  end
+
+  def self.xpto
+    square(10) + 56 / 2
   end
 end
--- test/algorithm_test.rb
+++ test/algorithm_test.rb
@@ -5,13 +5,21 @@ require 'algorithm.rb'
 require 'test/unit'
 
 class AlgorithmTest < Test::Unit::TestCase
-  def test_true
-    assert true
+  def test_call_square
+    assert Algorithm.square(10)
   end
 
-  def test_square
-    #assert true
-    Algorithm.square
+  def test_square_5
+    assert_equal 25, Algorithm.square(5)
+  end
+
+  def test_square_5
+    assert_equal 900, Algorithm.square(30)
+  end
+
+  def test_xpto
+    number = 10 * 10 + 56 / 2
+    assert_equal number, Algorithm.xpto
   end
 end
 
